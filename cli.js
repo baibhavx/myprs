6e38857232eacb17330c0cf5bdd5323b8544cc0e
baibhavx <baibhavx@gmail.com>
2018-11-06 22:46:17 -0800

    Use git util packages to parse url and username

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const findUp = require("find-up");
 const parse = require("parse-git-config");
 const getGitUsername = require("git-username");
-const readLine = require("readline");
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -13,25 +13,12 @@ const rl = readline.createInterface({
 
 const config = parse.sync();
 
-const forkRemote = "origin";
-const teamRemote = "upstream";
-
-// const parseURL = rawString => {
-//   const urlWithDotGit = rawString.split("=")[1].trim();
-//   return urlWithDotGit.substring(0, urlWithDotGit.length - 4);
-// };
-
-// const parseUsername = rawUsername => {
-//   const dotComIndex = rawUsername.indexOf(".com");
-//   rawUsername = rawUsername.slice(dotComIndex + 5);
-//   const indexOfSlash = rawUsername.indexOf("/");
-//   return rawUsername.slice(0, indexOfSlash);
-// };
-
 const getUsersPrUrlForRepo = (url, username) =>
   `${url}/pulls?utf8=%E2%9C%93&q=+is%3Apr+author%3A${username}+`;
 
 const findDotGitPath = () => findUp(".git");
+
+const removeDotGit = url => url.slice(0, url.length - 4);
 
 const openUsersPrInBrowser = (rawUrl, username) => {
   const url = rawUrl.substring(0, rawUrl.length - 4);
@@ -53,65 +40,43 @@ const execute = () => {
         remoteElems = remoteElems.split("");
         remoteElems.pop();
         remoteElems.shift();
-        const remoteName = remoteElems.join("");
-        // console.log(remoteName);
-        // console.log(repoStats);
+        const remoteName = remoteElems.join("").trim();
         repoStats[remoteName] = gitConfig[key];
-        // return repoStats;
       }
       return repoStats;
     }, {});
 
-    if (gitConfigInfo["origin"] && gitConfigInfo["upstream"]) {
-      console.log(`Username: ${username}`);
-      console.log(`Fork Remote Name: ${forkRemote}`);
-      console.log(`Parent Remote Name: ${teamRemote}`);
-      rl.question("Proceed with this config (y/n)", answer => {
-        if (answer.toLowerCase() === "y") {
-          // open the repo in a new broswer
-          openUsersPrInBrowser(gitConfigInfo["upstream"].url, username);
-        }
-      });
+    if (!gitConfigInfo["origin"]) {
+      console.error("Cannot find origin");
+      process.exit(0);
     }
-    console.error(`ERR: Remotes "upstream" and "origin" is required.`);
+    const originUrl = removeDotGit(gitConfigInfo["origin"].url);
 
-    // console.log(gitConfigInfo);
-    // console.log(username);
+    const remoteNames = Object.keys(gitConfigInfo);
 
+    console.log(`**Found remotes: ${remoteNames.join(" ")}`);
+
+    if (remoteNames.includes("upstream")) {
+      const upstreamUrl = removeDotGit(gitConfigInfo["upstream"].url);
+
+      console.log(`
+      **
+      Username:     ${username}
+      Origin URL:   ${originUrl}
+      Upstream URL: ${upstreamUrl} 
+      **
+      `);
+
+      const prURL = getUsersPrUrlForRepo(upstreamUrl, username);
+      opn(prURL);
+      process.exit(0);
+    } else {
+      console.error(
+        "Your use case is not supported yet. Remotes upstream and origin is required"
+      );
+    }
     process.exit(0);
-    // else {
-    // fs.readFile(`${path}/config`, "utf-8", function(err, data) {
-    //   const lines = data.split("\n");
-
-    //   const indexOfUpstreamUrl =
-    //     lines.findIndex(string => string.includes("upstream")) + 1;
-    //   const url = parseURL(lines[indexOfUpstreamUrl]);
-
-    //   const indexOfOriginUrl =
-    //     lines.findIndex(string => string.includes("origin")) + 1;
-    //   const username = parseUsername(lines[indexOfOriginUrl]);
-
-    //   opn(getUsersPrUrlForRepo(url, username));
-    //   process.exit(0);
-    // });
-    // }
   });
 };
-
-// let output = {
-//   core: {
-//     repositoryformatversion: "0",
-//     filemode: true,
-//     bare: false,
-//     logallrefupdates: true,
-//     ignorecase: true,
-//     precomposeunicode: true
-//   },
-//   'remote "origin"': {
-//     url: "git@github.com:baibhavx/myprs.git",
-//     fetch: "+refs/heads/*:refs/remotes/origin/*"
-//   },
-//   'branch "master"': { remote: "origin", merge: "refs/heads/master" }
-// };
 
 execute();
